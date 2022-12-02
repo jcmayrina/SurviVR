aeae58abebd42c966fe3003d2413c0793733da0e
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2022-12-02 23:00:00 +0800

    FIREEEEEEEE

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,70 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-
+    public PlayerInputActions playerControls;
     Rigidbody myRb;
-    Vector2 moveInput;
-
+    Vector2 moveDirection = Vector2.zero;
+    private InputAction move;
+    private InputAction fire;
+    public Image img;
+    private Camera cam;
+    private PlayerUI playerUI;
+    //private InputManager inputManager;
+    [SerializeField] private float maxDistance = 3f;
+    [SerializeField] private LayerMask mask;
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+        
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+    }
     void Start()
     {
         myRb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        playerUI = GetComponent<PlayerUI>();
     }
     void Update(){
-        Playermove();
+        moveDirection = move.ReadValue<Vector2>();
+        playerUI.UpdateText(string.Empty);
+        img.gameObject.SetActive(false);
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hitInfo;
+        Debug.DrawRay(ray.origin, ray.direction * maxDistance);
+        if(Physics.Raycast(ray, out hitInfo, maxDistance, mask)) {
+            if(hitInfo.collider.GetComponent<Interactable>() != null) {
+                playerUI.UpdateText(hitInfo.collider.GetComponent<Interactable>().promptMessage);
+                img.gameObject.SetActive(true);
+            }
+        }
     }
-    void Playermove(){
-        //float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
-        //Vector3 direction = new Vector3(horizontal,0,vertical);
-        //velocity = Camera.main.transform.TransformDirection(velocity);
-        //velocity.y -= gravity;
-        //controller.Move(velocity * Time.deltaTime);
-        Vector3 playerVelocity = new Vector3(moveInput.x * speed, myRb.velocity.y, moveInput.y * speed);
-        myRb.velocity = transform.TransformDirection(playerVelocity);
+    private void FixedUpdate(){
+        myRb.velocity = new Vector3(moveDirection.x * speed, myRb.velocity.y,moveDirection.y * speed);
     }
-    void OnMove(InputValue value){
-        moveInput = value.Get<Vector2>();
+    private void Fire(InputAction.CallbackContext context){
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hitInfo;
+        Debug.DrawRay(ray.origin, ray.direction * maxDistance);
+        if(Physics.Raycast(ray, out hitInfo, maxDistance, mask)) {
+            if(hitInfo.collider.GetComponent<Interactable>() != null) {
+                Debug.Log("Fire"+hitInfo.collider.name);
+            }
+        }
     }
 }
