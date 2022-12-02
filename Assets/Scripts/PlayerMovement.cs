d2a3bc9e9042e5510dea3ceb0f45c72074ef1257
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2022-12-02 20:58:04 +0800

    new movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,28 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float gravity = 10f;
-    public InputAction playerControls;
-    private CharacterController controller;
+
+    Rigidbody myRb;
+    Vector2 moveInput;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        myRb = GetComponent<Rigidbody>();
     }
     void Update(){
         Playermove();
     }
     void Playermove(){
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontal,0,vertical);
-        Vector3 velocity = direction*speed;
-        velocity = Camera.main.transform.TransformDirection(velocity);
-        velocity.y -= gravity;
-        controller.Move(velocity * Time.deltaTime);
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        //Vector3 direction = new Vector3(horizontal,0,vertical);
+        //velocity = Camera.main.transform.TransformDirection(velocity);
+        //velocity.y -= gravity;
+        //controller.Move(velocity * Time.deltaTime);
+        Vector3 playerVelocity = new Vector3(moveInput.x * speed, myRb.velocity.y, moveInput.y * speed);
+        myRb.velocity = transform.TransformDirection(playerVelocity);
+    }
+    void OnMove(InputValue value){
+        moveInput = value.Get<Vector2>();
     }
 }
